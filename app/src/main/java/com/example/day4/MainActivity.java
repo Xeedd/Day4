a67401e501994964d08714ab36f320914e1583aa
RPLACSIE <RPLACSIE@RPLACSIE-7400.ro.oracle.com>
2020-10-16 15:40:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/day4/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.day4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekbar;
+    private TextView txtMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekbar = findViewById(R.id.seekBar);
+        txtMessage = findViewById(R.id.txtMessage);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                 if(progress >=40 && progress <= 45){
+                     txtMessage.setVisibility(View.VISIBLE);
+                 } else
+                     txtMessage.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
